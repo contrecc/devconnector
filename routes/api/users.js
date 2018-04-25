2bdcb0fdc0d737b01d67bfe87023ed9f83913341
Colin Contreary <colinccontreary@gmail.com>
2018-04-24 18:39:59 -0700

    Added POST login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,35 +15,61 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc    Register a user
 // @access  Public
 router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email })
-    .then(user => {
-      if(user) {
-        return res.status(400).json({email: 'Email already exists'});
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size of image
+        r: 'pg', // Rating of image
+        d: 'mm' // Default (mm is blank picture)
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   POST api/users/login
+// @desc    Login a user / Returning JWT token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User email not found' });
+    }
+
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      //.compare returns a true/false value
+      if (isMatch) {
+        res.json({msg: 'Success'});
       } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: '200', // Size of image
-          r: 'pg',   // Rating of image
-          d: 'mm'   // Default (mm is blank picture)
-        });
-
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
+        return res.status(400).json({ password: 'Password incorrect' });
       }
     });
+  });
 });
 
 module.exports = router;
