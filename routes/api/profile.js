e8197356c8cff1a6a84c608e2cec3d57dde0f9ff
Colin Contreary <colinccontreary@gmail.com>
2018-04-27 18:48:54 -0700

    refactor profile POST route to prevent a user updating profile to have another user's handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,51 +113,88 @@ router.post(
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-      profileFields.githubusername = req.body.githubusername;
+
+    const standardFields = [
+      'handle',
+      'company',
+      'website',
+      'location',
+      'bio',
+      'status',
+      'githubusername'
+    ];
+
+    const socialFields = [
+      'youtube',
+      'twitter',
+      'facebook',
+      'linkedin',
+      'instagram'
+    ];
+
+    // Assign standard profile fields
+    standardFields.forEach(field => {
+      if (req.body[field]) profileFields[field] = req.body[field];
+    });
+
+    // Assign social profile fields
+    profileFields.social = {};
+    socialFields.forEach(field => {
+      if (req.body[field]) profileFields.social[field] = req.body[field];
+    });
 
     //Skills - Split into an array
     if (typeof req.body.skills !== 'undefined') {
       profileFields.skills = req.body.skills.split(',');
     }
 
-    //Social
-    profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    Profile.findOne({ user: profileFields.user })
+      .then(profile => {
+        Profile.findOne({ handle: profileFields.handle })
+          .then(profileWithHandle => {
+            if (profileWithHandle) {
+              if (profileWithHandle.user !== profileFields.user) {
+                errors.handle = 'That handle already exists';
+                return res.status(400).json(errors);
+              }
+            }
 
-    Profile.findOne({ user: req.user.id }).then(profile => {
-      if (profile) {
-        // If a profile exits, then we are updating it
-        Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true }
-        ).then(profile => res.json(profile));
-      } else {
-        // If no profile, then we are creating one
-
-        // Check if handle exists
-        Profile.findOne({ handle: profileFields.handle }).then(profile => {
-          if (profile) {
-            errors.handle = 'That handle already exists';
-            res.status(400).json(errors);
-          }
-
-          // Save Profile
-          new Profile(profileFields).save().then(profile => res.json(profile));
-        });
-      }
-    });
+            if (profile && !errors.handle) {
+              Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+              )
+                .then(profile => {
+                  res.json(profile);
+                })
+                .catch(err =>
+                  res.status(500).json({
+                    server: 'Something went wrong, please try again soon'
+                  })
+                );
+            } else if (!errors.handle) {
+              new Profile(profileFields)
+                .save()
+                .then(profile => res.json(profile))
+                .catch(err =>
+                  res.status(500).json({
+                    server: 'Something went wrong, please try again soon'
+                  })
+                );
+            }
+          })
+          .catch(err =>
+            res
+              .status(500)
+              .json({ server: 'Something went wrong, please try again soon' })
+          );
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ server: 'Something went wrong, please try again soon' })
+      );
   }
 );
 
@@ -256,7 +293,6 @@ router.delete(
   (req, res) => {
     Profile.findOne({ user: req.user.id })
       .then(profile => {
-       
         profile.education.remove({ _id: req.params.edu_id });
 
         profile
