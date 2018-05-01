290a2aa5301040efff94ab490a0c6e2ea123ab12
Colin Contreary <colinccontreary@gmail.com>
2018-04-30 17:47:33 -0700

    add Navbar changes to make conditional links work

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,8 +19,7 @@ class Navbar extends Component {
           <a
             href="#"
             onClick={this.onLogoutClick.bind(this)}
-            className="nav-link"
-          >
+            className="nav-link">
           <img className="rounded-circle" src={user.avatar} alt={user.name} style={{width: '25px', marginRight: '5px'}} title="You must have a Gravatar connected to your email to display an image" />
             Logout
           </a>
