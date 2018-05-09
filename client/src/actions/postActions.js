0f6e0b4c42a7e2b633365c1f7917892f64245738
Colin Contreary <colinccontreary@gmail.com>
2018-05-09 02:32:27 -0700

    create getPosts Action Creator and PostFeed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS } from './types';
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 
-// Add A Post
+// Add A Post Action Creator
 export const addPost = postData => dispatch => {
   axios
     .post('/api/posts', postData)
@@ -17,4 +17,30 @@ export const addPost = postData => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Get All Posts Action Creator
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set Loading State
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
 };
