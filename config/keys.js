7134c19364142958b23dc85ee06962b131ddecb1
Colin Contreary <colinccontreary@gmail.com>
2018-05-10 18:56:23 -0700

    Prepare for deploy to Heroku

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-//require('dotenv').config();
-
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.JWT_SECRET
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
